de3f2b0232e86515480e4a6d0bed0d8946545ec9
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-02-12 02:58:21 -0300

    07 OK

---
+++ desafio7.sql
@@ -0,0 +1,16 @@
+SELECT 
+a.artist_name AS artista,
+al.album_name AS album,
+COUNT(f.artists_id) AS seguidores
+
+FROM  SpotifyClone.artists AS a
+JOIN SpotifyClone.albuns AS al
+ON a.artists_id = al.artists_id
+
+JOIN SpotifyClone.followers AS f
+ON f.artists_id = al.artists_id
+
+group by album_id
+ORDER BY seguidores desc,
+artista ASC,
+album ASC;
